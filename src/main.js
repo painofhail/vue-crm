d0e3dc067d9e5ac80c00074669d00f847a9b7b0a
painofhail <painofhail@yandex.ru>
2021-07-26 20:06:31 +0300

    vuelidate auth form

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
+//Libraries
 import Vue from 'vue';
+import Vuelidate from 'vuelidate';
+
+//Sources
 import App from '@/App.vue';
 import '@/registerServiceWorker';
 import router from '@/router';
 import store from '@/store';
+
 import dateFilter from '@/filters/date.filter';
 import 'materialize-css/dist/js/materialize';
 
 Vue.config.productionTip = false;
+
+// =======================================================================
+// Libraries
+Vue.use(Vuelidate);
 
 // Filters
 Vue.filter('dateFilter', dateFilter);
--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,16 +1,35 @@
 <template>
-	<form class="card auth-card">
+	<form class="card auth-card" @submit.prevent="submitHandler">
 		<div class="card-content">
 			<span class="card-title">Домашняя бухгалтерия</span>
 			<div class="input-field">
-				<input id="email" type="text" class="validate">
+				<input
+					id="email" type="text"
+					v-model.trim="email"
+					:class="{ invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email) }"
+				>
 				<label for="email">Email</label>
-				<small class="helper-text invalid">Email</small>
+				<small class="helper-text invalid" v-if="($v.email.$dirty && !$v.email.required)">
+					Поле Email не должно быть пустым
+				</small>
+				<small class="helper-text invalid" v-else-if="($v.email.$dirty && !$v.email.email)">
+					Введите корректный e-mail
+				</small>
 			</div>
 			<div class="input-field">
-				<input id="password" type="password" class="validate">
+				<input
+					id="password"
+					type="password"
+					v-model="password"
+					:class="{ invalid: ($v.password.$dirty && !$v.password.required) || ($v.password.$dirty && !$v.password.minLength) }"
+				>
 				<label for="password">Пароль</label>
-				<small class="helper-text invalid">Password</small>
+				<small class="helper-text invalid" v-if="($v.password.$dirty && !$v.password.required)">
+					Введите пароль
+				</small>
+				<small class="helper-text invalid" v-else-if="($v.password.$dirty && !$v.password.minLength)">
+					Длина пароля не может быть меньше {{passwordLength}} символов
+				</small>
 			</div>
 		</div>
 		<div class="card-action">
@@ -21,7 +40,48 @@
 				</button>
 			</div>
 
-			<p class="center">Нет аккаунта?<a href="/"> Зарегистрироваться</a></p>
+			<p class="center">
+				Нет аккаунта?
+				<router-link to="/register"> Зарегистрироваться</router-link>
+			</p>
 		</div>
 	</form>
 </template>
+
+<script>
+	import {email, required, minLength} from 'vuelidate/lib/validators';
+
+	export default {
+		name: 'Auth',
+		data: () => ({
+			email: '',
+			password: ''
+		}),
+		validations: {
+			email: { email, required },
+			password: { required, minLength: minLength(6) }
+		},
+		computed: {
+			passwordLength () {
+				return this.$v.password.$params.minLength.min;
+			}
+		},
+		methods: {
+			submitHandler() {
+				if (this.$v.$invalid) {
+					this.$v.$touch();
+					return;
+				}
+
+				const formDate = {
+					email: this.email,
+					password: this.password
+				};
+
+				console.log(formDate);
+
+				this.$router.push("/");
+			}
+		}
+	}
+</script>
