3d86ed05a9e6f44eb866c1277648a6d5edbb3160
painofhail <painofhail@yandex.ru>
2021-08-06 10:51:07 +0300

    Detail

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,25 +1,58 @@
 <template>
 	<div>
-		<div>
+		<Loader v-if="loading" />
+
+		<div v-else-if="record">
 			<div class="breadcrumb-wrap">
-				<a href="/history" class="breadcrumb">История</a>
-				<a class="breadcrumb">
-					Расход
-				</a>
+				<router-link to="/history" class="breadcrumb">История</router-link>
+				<a @click.prevent class="breadcrumb">{{ recordType }}</a>
 			</div>
 			<div class="row">
 				<div class="col s12 m6">
-					<div class="card red">
+					<div class="card" :class="recordColor">
 						<div class="card-content white-text">
-							<p>Описание:</p>
-							<p>Сумма:</p>
-							<p>Категория:</p>
-
-							<small>12.12.12</small>
+							<p>Описание: {{ record.description }}</p>
+							<p>Сумма: {{ record.amount | currencyFilter() }}</p>
+							<p>Категория: {{ record.categoryName }}</p>
+							<small>{{ record.date | dateFilter('datetime') }}</small>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
+
+		<p v-else class="center"><strong>Запись с id = {{ recordId}} не найдена</strong></p>
 	</div>
 </template>
+
+<script>
+	export default {
+		name: 'Detail',
+		data: () => ({
+			loading: true,
+			record: null
+		}),
+		computed: {
+			recordId () {
+				return this.$route.params.id
+			},
+			recordType () {
+				return this.record.type === 'income' ? 'Доход' : 'Расход'
+			},
+			recordColor () {
+				return this.record.type === 'income' ? 'green' : 'red'
+			}
+		},
+		async mounted () {
+			const record = await this.$store.dispatch('fetchRecordById', this.recordId)
+			const category = await this.$store.dispatch('fetchCategoryById', record.categoryId)
+
+			this.record = {
+				...record,
+				categoryName: category.title
+			}
+
+			this.loading = false;
+		}
+	}
+</script>
